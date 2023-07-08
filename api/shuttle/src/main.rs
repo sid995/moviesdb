390a8d9d3d8da0a4f5da4896ba2812c9891efdbb
Siddharth Kundu <sk31395@gmail.com>
2023-07-08 19:07:57 +0530

    add health check endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -3,7 +3,7 @@ use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 
-use api_lib::health::version;
+use api_lib::health::health;
 
 #[shuttle_runtime::main]
 async fn actix_web(
@@ -14,7 +14,7 @@ async fn actix_web(
         .map_err(CustomError::new)?;
     let pool = actix_web::web::Data::new(pool);
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool).service(version);
+        cfg.app_data(pool).service(health);
     };
 
     Ok(config.into())
