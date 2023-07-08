88304800e0fba2699e0d9a193fc7f81f793f6b9e
Siddharth Kundu <sk31395@gmail.com>
2023-07-08 19:04:09 +0530

    move endpoints to a library

---
+++ api/lib/src/health.rs
@@ -0,0 +1,14 @@
+use actix_web::get;
+
+#[get("/version")]
+async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(db.get_ref())
+        .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,20 +1,9 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-
-    match result {
-        Ok(version) => version,
-        Err(e) => format!("Error: {:?}", e),
-    }
-}
+use api_lib::health::version;
 
 #[shuttle_runtime::main]
 async fn actix_web(
